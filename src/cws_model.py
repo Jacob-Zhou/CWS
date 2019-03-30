af95435bb0eb7ec643665b7fa3724640acd4779a
zysite <zysite@outlook.com>
2019-03-30 16:10:35 +0800

    Add subword representation

--- src/cws_model.py
+++ src/cws_model.py
@@ -4,8 +4,9 @@ import os
 
 import torch
 import torch.nn as nn
-from src.common import pad_index
-from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from src.common import pad_index, unk_index
+from torch.nn.utils.rnn import (pack_padded_sequence, pad_packed_sequence,
+                                pad_sequence)
 
 
 class CWSModel(nn.Module):
@@ -19,6 +20,7 @@ class CWSModel(nn.Module):
 
         self.emb_chars = None
         self.emb_bichars = None
+        self.emb_subwords = None
         self.emb_drop_layer = None
         self.lstm_layer = None
         self.ffn = None
@@ -29,15 +31,19 @@ class CWSModel(nn.Module):
         return self._name
 
     # create and init all the models needed according to config
-    def init_models(self, char_dict_size, bichar_dict_size, label_dict_size):
+    def init_models(self, char_dict_size, bichar_dict_size,
+                    subword_dict_size, label_dict_size):
         assert char_dict_size > 0
         assert bichar_dict_size > 0
+        assert subword_dict_size > 0
         assert label_dict_size > 0
 
         self.emb_chars = nn.Embedding(num_embeddings=char_dict_size,
                                       embedding_dim=self._conf.char_emb_dim)
         self.emb_bichars = nn.Embedding(num_embeddings=bichar_dict_size,
                                         embedding_dim=self._conf.char_emb_dim)
+        self.emb_subwords = nn.Embedding(num_embeddings=subword_dict_size,
+                                         embedding_dim=self._conf.subword_emb_dim)
         self.emb_drop_layer = nn.Dropout(self._conf.emb_dropout)
 
         self.lstm_layer = nn.LSTM(input_size=self._conf.char_emb_dim*2,
@@ -47,7 +53,7 @@ class CWSModel(nn.Module):
                                   num_layers=self._conf.lstm_layer_num,
                                   dropout=self._conf.lstm_dropout)
 
-        self.ffn = nn.Linear(self._conf.lstm_hidden_dim,
+        self.ffn = nn.Linear(self._conf.lstm_hidden_dim + self._conf.subword_emb_dim,
                              label_dict_size)
         self.log_softmax = nn.LogSoftmax(dim=-1)
         self.loss_func = nn.NLLLoss()
@@ -56,39 +62,44 @@ class CWSModel(nn.Module):
     def reset_parameters(self):
         nn.init.xavier_uniform_(self.ffn.weight)
 
-    def put_models_on_gpu_if_need(self):
-        if not self._use_cuda:
-            return
-        self.cuda()
-
-    def forward(self, chars, bichars):
+    def forward(self, chars, bichars, subwords):
         mask = chars.ne(pad_index)
+        subword_mask = subwords.ne(pad_index) & subwords.ne(unk_index)
+        batch_size, seq_len, _ = subwords.shape
         lens = mask.sum(1)
 
         emb_ch = self.emb_chars(chars)
         emb_bich = self.emb_bichars(bichars)
+        emb_subword = self.emb_subwords(subwords)
         x = self.emb_drop_layer(torch.cat((emb_ch, emb_bich), -1))
 
         sorted_lens, sorted_indices = torch.sort(lens, descending=True)
         inverse_indices = sorted_indices.argsort()
         x = pack_padded_sequence(x[sorted_indices], sorted_lens, True)
-
         x, _ = self.lstm_layer(x)
         x, _ = pad_packed_sequence(x, True)
         x = x[inverse_indices].transpose(0, 1)
+
         x = x.unsqueeze(1) - x
         x_f, x_b = x.chunk(2, dim=-1)
-
         x_f = x_f[1:-1, :-2].permute(2, 1, 0, 3)
         x_b = x_b[1:-1, 2:].permute(2, 0, 1, 3)
-        x_span = torch.cat([x_f, x_b], -1)
-        x = self.ffn(x_span)
+        x_span = torch.cat([x_f, x_b], dim=-1)
+        x_span = pad_sequence([
+            x_span.diagonal(offset=i, dim1=1, dim2=2).permute(2, 0, 1)
+            for i in range(min(self._conf.max_word_length, seq_len))
+        ], True)
+        x_span = x_span.transpose(0, 2)
+
+        x = torch.cat([x_span, emb_subword], dim=-1)
+        x = self.ffn(x)
+        x = x.masked_fill_(~subword_mask.unsqueeze(-1), 0)
         x = self.log_softmax(x)
 
         return x
 
-    def get_loss(self, out, target):
-        return self.loss_func(out, target)
+    def get_loss(self, out, target, mask):
+        return self.loss_func(out[mask], target[mask])
 
     def load_model(self, path, eval_num):
         path = os.path.join(path, 'models.%s.%d' % (self.name, eval_num))
