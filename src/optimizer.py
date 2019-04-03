d357691692952f032503d84ec0f92ea0d0b6519e
zysite <zysite@outlook.com>
2019-04-03 15:24:39 +0800

    Replace the Adam optimizer with SGD

--- src/optimizer.py
+++ src/optimizer.py
@@ -6,9 +6,13 @@ from torch import optim
 class Optimizer:
 
     def __init__(self, params_to_optimize, conf):
-        self.optim = optim.Adam(params=params_to_optimize,
-                                lr=conf.learning_rate)
+        def annealing(x): return conf.lr * ((1 - conf.decay)**x)
+        self.optimizer = optim.SGD(params=params_to_optimize,
+                                   lr=conf.lr)
+        self.scheduler = optim.lr_scheduler.LambdaLR(optimizer=self.optimizer,
+                                                     lr_lambda=annealing)
 
     def step(self):
-        self.optim.step()
-        self.optim.zero_grad()
+        self.optimizer.step()
+        self.scheduler.step()
+        self.optimizer.zero_grad()
