f515e80f6d827bb35731c5cd1ada9e5850d73f86
zysite <zysite@outlook.com>
2019-04-05 10:39:07 +0800

    Add Embedding

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,8 +2,9 @@
 
 from .bucketing import Bucketing
 from .dataset import Dataset
+from .embedding import Embedding
 from .instance import Instance
 from .vocab import VocabDict
 
 
-__all__ = ['Bucketing', 'Dataset', 'Instance', 'VocabDict']
+__all__ = ['Bucketing', 'Dataset', 'Embedding', 'Instance', 'VocabDict']
