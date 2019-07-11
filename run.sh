9373db5de9ac32e2307d37509df5bb028ddc39b6
jacob_zhou <Jacob_Zhou@outlook.com>
2019-07-11 15:53:01 +0800

    none

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-cudaN=7
+cudaN=5
 exe="nohup python -u main.py --device $cudaN --config config.ini --path=exp/ctb9-mtl-bert"
 # $exe  --is-train > results/log.create-dict 2>&1 &
-$exe --is-dictionary-exist --is-train > results/log.mtl.bert 2>&1 &
+# $exe --is-dictionary-exist --is-train > results/log.mtl.bert 2>&1 &
 
-# $exe  --is-test > log.test 2>&1 &
+$exe  --is-test > log.test 2>&1 &
