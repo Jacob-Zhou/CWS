47178f5556fe0b6e5aa02c81cea2197e21cf143b
jacob_zhou <Jacob_Zhou@outlook.com>
2019-07-12 19:28:27 +0800

    add ini

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 cudaN=5
-exe="nohup python -u main.py --device $cudaN --config config.ini --path=exp/ctb9-mtl-bert"
-# $exe  --is-train > results/log.create-dict 2>&1 &
-# $exe --is-dictionary-exist --is-train > results/log.mtl.bert 2>&1 &
+exe="nohup python -u main.py"
+# $exe --device 2 --config config_as.ini --path=exp/as.bert --is-train > results/log.create-dict.as.bert 2>&1 &
+# $exe --device 1 --config config_cityu.ini --path=exp/cityu.bert --is-train > results/log.create-dict.cityu.bert 2>&1 &
 
-$exe  --is-test > log.test 2>&1 &
+$exe --device 2 --config config_as.ini --path=exp/as.bert --is-dictionary-exist --is-train > results/log.as.bert 2>&1 &
+$exe --device 1 --config config_cityu.ini --path=exp/cityu.bert --is-dictionary-exist --is-train > results/log.cityu.bert 2>&1 &
