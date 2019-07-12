1d2710cc12122541c5e5d18162c417150e10d33e
jacob_zhou <Jacob_Zhou@outlook.com>
2019-07-12 19:47:11 +0800

    nothing important

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ exe="nohup python -u main.py"
 # $exe --device 2 --config config_as.ini --path=exp/as.bert --is-train > results/log.create-dict.as.bert 2>&1 &
 # $exe --device 1 --config config_cityu.ini --path=exp/cityu.bert --is-train > results/log.create-dict.cityu.bert 2>&1 &
 
-$exe --device 2 --config config_as.ini --path=exp/as.bert --is-dictionary-exist --is-train > results/log.as.bert 2>&1 &
-$exe --device 1 --config config_cityu.ini --path=exp/cityu.bert --is-dictionary-exist --is-train > results/log.cityu.bert 2>&1 &
+# $exe --device 2 --config config_as.ini --path=exp/as.bert --is-dictionary-exist --is-train > results/log.as.bert 2>&1 &
+$exe --device 3 --config config_cityu.ini --path=exp/cityu.bert --is-dictionary-exist --is-train > results/log.cityu.bert 2>&1 &
