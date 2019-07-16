009c2e4e15c05ad55377bb27389f820134af1bab
jacob_zhou <Jacob_Zhou@outlook.com>
2019-07-16 17:13:55 +0800

    update shell

--- run_gpu5.sh
+++ run_gpu5.sh
@@ -1,5 +1,5 @@
 exe="nohup python -u main.py"
 
-# $exe --device 5 --config config_sighan10.ini --path=exp/sighan10 --is-dictionary-exist --is-train > results/log.sighan10 2>&1
-$exe --device 3 --config config_sighan10.bert.ini --path=exp/sighan10.bert --is-train > results/log.sighan10.bert 2>&1
-$exe --device 3 --config config_sighan10.bert.ini --path=exp/sighan10.bert --is-dictionary-exist --is-train > results/log.sighan10.bert 2>&1
+# $exe --device 5 --config config_sighan10.ini --path=exp/sighan10.cd --is-dictionary-exist --is-train > results/log.cd.sighan10 2>&1
+$exe --device 3 --config config_sighan10.bert.ini --path=exp/sighan10.cd.bert --is-train > results/log.sighan10.cd.bert 2>&1
+$exe --device 3 --config config_sighan10.bert.ini --path=exp/sighan10.cd.bert --is-dictionary-exist --is-train > results/log.sighan10.cd.bert 2>&1
