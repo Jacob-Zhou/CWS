5d4e8ebc0f53a21b223172ee91dad44af255da47
jacob_zhou <Jacob_Zhou@outlook.com>
2019-07-16 11:00:40 +0800

    change shell

--- run_gpu4.sh
+++ run_gpu4.sh
@@ -3,5 +3,5 @@ exe="nohup python -u main.py"
 # $exe --device 4 --config config_zx.ini --path=exp/zx --is-dictionary-exist --is-train > results/log.zx 2>&1 
 # $exe --device 4 --config config_zx_test.ini --path=exp/zx --is-test > results/log.test.zx 2>&1 
 # $exe --device 4 --config config_zx.bert.ini --path=exp/zx.bert --is-train > results/log.create-dict.zx.bert 2>&1 
-# $exe --device 4 --config config_zx.bert.ini --path=exp/zx.bert --is-dictionary-exist --is-train > results/log.zx.bert 2>&1 
-$exe --device 4 --config config_zx_test.bert.ini --path=exp/zx.bert --is-test > results/log.test.zx.bert 2>&1 
+$exe --device 3 --config config_zx.bert.ini --path=exp/zx.cd.bert --is-dictionary-exist --is-train > results/log.zx.cd.bert 2>&1 
+$exe --device 3 --config config_zx_test.bert.ini --path=exp/zx.cd.bert --is-test > results/log.test.zx.cd.bert 2>&1 &
