e45520066992f51e8ba3eb7880a95b077043dd6d
jacob_zhou <Jacob_Zhou@outlook.com>
2019-07-17 21:41:30 +0800

    update train config

--- run_gpu4.sh
+++ run_gpu4.sh
@@ -1,7 +1,7 @@
 exe="nohup python -u main.py"
 
 # $exe --device 4 --config config_zx.ini --path=exp/zx.cd --is-dictionary-exist --is-train > results/log.cd.zx 2>&1 
-$exe --device 4 --config config_zx_test.ini --path=exp/zx.cd --is-test > results/log.test.cd.zx 2>&1 
+# $exe --device 4 --config config_zx_test.ini --path=exp/zx.cd --is-test > results/log.test.cd.zx 2>&1 
 # $exe --device 4 --config config_zx.bert.ini --path=exp/zx.cd.bert --is-train > results/log.create-dict.zx.bert 2>&1 
-# $exe --device 3 --config config_zx.bert.ini --path=exp/zx.cd.bert --is-dictionary-exist --is-train > results/log.zx.cd.bert 2>&1 
-$exe --device 3 --config config_zx_test.bert.ini --path=exp/zx.cd.bert --is-test > results/log.test.zx.cd.bert 2>&1 &
+# $exe --device 4 --config config_zx.bert.ini --path=exp/zx.cd.bert --is-dictionary-exist --is-train > results/log.zx.cd.bert.dic_cat 2>&1 
+$exe --device 4 --config config_zx_test.bert.ini --path=exp/zx.cd.bert --is-test > results/log.test.zx.cd.bert.lr_4 2>&1 &
